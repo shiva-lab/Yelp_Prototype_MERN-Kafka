3d52bed6ae2aaba59b1e3d0c5526a19543bf1898
shiva-lab <shiva.pandey112019@gmail.com>
2020-11-01 19:24:26 -0600

    Implemented Pagination on User View Menu
    User will have an ability to paginate through the menu.

--- frontend/src/component/userview/Order/uviewmenu.js
+++ frontend/src/component/userview/Order/uviewmenu.js
@@ -2,42 +2,69 @@ import React, { Component, useState } from "react";
 import { Redirect } from 'react-router';
 import cookie from "react-cookies";
 import Navbar from "../uNavbar";
+import {paginate, pages} from '../../../helperFunctions/paginate'
+import axios from "axios"
 
 // import Modal from 'react-modal';
 class uviewmenu extends React.Component {
   constructor(props) {
     super();
     this.state = {
-      items: [],
+      menu: [],
+      filteredMenu:[]
     };
   }
 
   componentDidMount() {
-    const self = this;
     const restaurant_id = localStorage.getItem("restaurant_id_allrest");
     const data = { restaurant_id };
-    fetch("/uviewmenu", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        if (response.status >= 400) {
-          throw new Error("Bad response from server");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log("data")
-        console.log(data)
-        self.setState({ items: data});
-      })
-      .catch((err) => {
-        console.log("caught it!", err);
-      });
-  }
+    axios.defaults.withCredentials = true;
+    const self = this;
+    axios.post('/uviewmenu',data)
+    .then((response) => {
+      if (response.status === 200) {
+        console.log("Printing response",response)
+        console.log("Printing User Data",response.data)
+          this.setState({
+            menu: response.data,
+            filteredMenu : paginate(response.data,1,10),
+            pages: pages(response.data, 10)
+
+          })
+          console.log(pages);
+      } else {
+          console.log("error");
+      }
+  });
+}
+
+
+
+
+
+
+  //   fetch("/uviewmenu", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(data),
+  //   })
+  //     .then((response) => {
+  //       if (response.status >= 400) {
+  //         throw new Error("Bad response from server");
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       console.log("data")
+  //       console.log(data)
+  //       self.setState({ items: data});
+  //     })
+  //     .catch((err) => {
+  //       console.log("caught it!", err);
+  //     });
+  // }
 
 
   handleClick(_id, itemname, restaurant_id, price, path) {
@@ -78,6 +105,11 @@ class uviewmenu extends React.Component {
 
   }
 
+  paginatinon = (e) => {
+    this.setState({
+        filteredMenu: paginate(this.state.menu,e, 10)
+    })
+}
 
   render() {
     let redirectVar = null;
@@ -85,6 +117,48 @@ class uviewmenu extends React.Component {
     if (!cookie.load("cookie1")) {
       redirectVar = <Redirect to="/" />;
     }
+    let links = [];
+    if (this.state.pages > 0) {
+        //console.log(this.state.pages);
+        for (let i = 1; i <= this.state.pages; i++) {
+            links.push(<li className="page-item" key={i}><a className="page-link" onClick={() => { this.paginatinon(i) }}>
+                {i}
+            </a></li>
+            )
+        }
+    }
+
+    let userdata = this.state.filteredMenu.map(item => {
+      return(
+    <tr>
+                      <td>
+                                <img
+                                  src={item.path}
+                                  width={150}
+                                  height={120}
+                                  mode="fit"
+                                />
+                              </td>
+                              <td>{item.itemname}{' '}</td>
+                              <td>{item.item_description}{' '}</td>
+                              <td>{item.price}</td>
+                              <td>{item.itemcategory}</td>
+                              <td>
+                                <button
+                             onClick={this.handleClick(item._id, item.itemname, item.restaurant_id, item.price, item.path)}
+                                >
+                                  Add to cart
+                                </button>
+                              </td>
+                              </tr>
+                           
+    )})
+
+
+
+
+
+
     return (
       <div>
         {redirectVar}
@@ -111,35 +185,12 @@ class uviewmenu extends React.Component {
                           </tr>
                         </thead>
                         <tbody>
-                          {this.state.items.map(item => (
-                            <tr>
-                              <td>
-                                <img
-                                  src={item.path}
-                                  width={150}
-                                  height={120}
-                                  mode="fit"
-                                />
-                              </td>
-                              <td>
-                                {item.itemname}
-                                {' '}
-                              </td>
-                              <td>
-                                {item.item_description}
-                                {' '}
-                              </td>
-                              <td>{item.price}</td>
-                              <td>{item.itemcategory}</td>
-                              <td>
-                                <button
-                                  onClick={this.handleClick(item._id, item.itemname, item.restaurant_id, item.price, item.path)}
-                                >
-                                  Add to cart
-                                </button>
-                              </td>
-                            </tr>
-                          ))}
+                        {userdata}
+                            <ul className="pagination">
+                            {links}
+                            </ul>
+                        
+                        
                         </tbody>
                       </table>
                     </div>
